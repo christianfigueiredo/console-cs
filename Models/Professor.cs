fe43fccbed3f16ba0c8c13e2bfbae3910ce10ef5
christianfigueiredo <developer.figueiredo@gmail.com>
2022-04-22 20:56:06 +0100

    propriedade DateTime inserida

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -3,6 +3,6 @@ namespace console_cs.Models
     public class Professor
     {
         public int IdProfessor { get; set; }
-        public string DataInicio { get; set; }
+        public DateTime DataInicio { get; set; }
     }
 }
